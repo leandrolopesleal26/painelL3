67dea0ed8e1dbb46385f986764711b5b8bfb7dda
leandrolopesleal26 <leandrolopesleal26@gmail.com>
2022-01-14 08:36:04 -0300

    Inicio do repositorio PainelL3

---
+++ app/biblioteca_SEA.py
@@ -0,0 +1,407 @@
+
+# !/usr/bin/env/ python3
+# -*- coding:utf-8 -*-
+
+from gtts import gTTS  # importamos o modúlo gTTS
+import pygame  # Permite a utilização do modulo para execução de mp3
+import time  # Funções de tempo
+import RPi.GPIO as GPIO
+import smbus  # para funcionamento dos módulos com interface I2C
+import spidev
+import os  # Permite a execução de comandos do sistema operacional dentro do script Ex.: os.system('sudo reboot now')
+import threading  # Modulo superior Para executar as threads
+
+
+GPIO.setwarnings(False)  # desabilita mensagens de aviso
+GPIO.setmode(GPIO.BCM)  # Modo de endereço dos pinos BCM
+spi = spidev.SpiDev()  # Parametros da configuração da comunicação SPI (Entrada Portas Analógcas)
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+pygame.init()  # Inicia o pygame para uso do módulo de voz
+pygame.mixer.music.set_volume(0.1)  # Ajusta o volume geral das narrações do modulo de voz
+bus = smbus.SMBus(1)  # Parametros de configuração do módulo MCP23017 - SAIDA DOS RELÊS via I2C
+MCP23017 = 0X20  # Endereço do módulo de saidas dos reles
+MCP3008 = 0
+bus.write_byte_data(MCP23017, 0x00, 0x00)  # defina todo GPA como saida 0x00
+bus.write_byte_data(MCP23017, 0x01, 0x00)  # defina todo GPB como saida 0x01
+
+# Define os pinos que serão entradas:
+
+GPIO.setup(17, GPIO.IN)  # GPIO0 (in 1)
+GPIO.setup(18, GPIO.IN)  # GPIO1 (in 2)
+GPIO.setup(27, GPIO.IN)  # GPIO2 (in 3)
+GPIO.setup(22, GPIO.IN)  # GPIO3 (in 4)
+GPIO.setup(23, GPIO.IN)  # gpio4 (in 5)
+GPIO.setup(24, GPIO.IN)  # GPIO5 (in 6)
+GPIO.setup(25, GPIO.IN)  # GPIO6 (in 7)
+GPIO.setup(4, GPIO.IN)   # GPIO7 (in 8)
+
+IN1 = GPIO.input(17)
+IN2 = GPIO.input(18)
+IN3 = GPIO.input(27)
+IN4 = GPIO.input(22)
+IN5 = GPIO.input(23)
+IN6 = GPIO.input(24)
+IN7 = GPIO.input(25)
+IN8 = GPIO.input(4)
+
+saidaA = 0b00000000  # Zera as saidas do port A (saidas do rele 1 ao rele 8 )
+saidaB = 0b00000000  # Zera as saidas do port B (saidas dos reles 9 e 10 e dos transistors 11,12,13)
+
+bus.write_byte_data(MCP23017, 0x015, 0)  # Zera saidas do port B
+bus.write_byte_data(MCP23017, 0x014, 0)  # Coloca todas as saidas do PORT A em 0
+
+
+########################## Tradução dos dias da semana para módulo de voz caso usar GTTS (somente online)
+
+if (time.strftime("%A") == "Sunday"):
+    dia_da_semana = "Domingo"
+
+if (time.strftime("%A") == "Monday"):
+    dia_da_semana = "Segunda feira"
+
+if (time.strftime("%A") == "Tuesday"):
+    dia_da_semana = "Terça feira"
+
+if (time.strftime("%A") == "Wednesday"):
+    dia_da_semana = "Quarta feira"
+
+if (time.strftime("%A") == "Thursday"):
+    dia_da_semana = "Quinta feira"
+
+if (time.strftime("%A") == "Friday"):
+    dia_da_semana = "Sexta feira"
+
+if (time.strftime("%A") == "Saturday"):
+    dia_da_semana = "Sábado"
+
+hs = time.strftime("%H:%M:%S")  # Hora completa para registro de Log
+hora = time.strftime('%H:%M')
+h = int(time.strftime('%H'))
+dia_mes = time.strftime("%d")
+y = time.strftime("%Y")
+m = time.strftime("%m")
+data = time.strftime('%d/%m/%y')
+
+
+# Rotina para GTTS (API Text to Spech do Google)
+
+def dia_e_hora(hora):
+    if (h < 12):
+        periodo = "Bom dia!"
+
+    if (h > 12 and h < 18):
+        periodo = "Boa tarde!"
+
+    if (h >= 18 and h <= 23):
+        periodo = "Boa noite!"
+
+    voz = gTTS(periodo, lang="pt")
+    voz.save("periodo.mp3")
+    voz = gTTS("Agora são", lang="pt")
+    voz.save("voz_hora.mp3")
+    voz = gTTS(hora, lang="pt")
+    voz.save("voz_hora1.mp3")
+
+    pygame.mixer.music.load('voz_hora.mp3')
+    pygame.mixer.music.play()
+    time.sleep(1.2)
+    pygame.mixer.music.load('voz_hora1.mp3')
+    pygame.mixer.music.play()
+    time.sleep(2.5)
+
+
+# Rotina para GTTS
+
+def dia_semana(dia_da_semana):
+    voz = gTTS("e hoje é", lang="pt")
+    voz.save("hoje_e.mp3")
+    voz = gTTS(dia_da_semana, lang="pt")
+    voz.save("dia_semana.mp3")
+
+    pygame.mixer.music.load('hoje_e.mp3')
+    pygame.mixer.music.play()
+    time.sleep(1.05)
+    pygame.mixer.music.load('dia_semana.mp3')
+    pygame.mixer.music.play()
+    time.sleep(2)
+
+
+# Rotina para GTTS
+
+def dia_do_mes(dia_mes):
+    voz = gTTS(dia_mes, lang="pt")
+    voz.save("dia_mes.mp3")
+    pygame.mixer.music.load('dia_mes.mp3')
+    pygame.mixer.music.play()
+    time.sleep(2)
+
+# Rotina para GTTS
+
+def narrador(mensagem):
+    try:
+
+        voz = gTTS(mensagem, lang="pt")  # guardamos o nosso texto na variavel voz
+        voz.save("mensagem.mp3")  # salvamos com o comando save em mp3
+        pygame.mixer.music.load('mensagem.mp3')
+        pygame.mixer.music.play()
+        print("Reproduzindo Texto no narrador")
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+        print("terminou o narração")
+
+    except:
+
+        print("narrador não esta funcionando")
+
+
+# Função que obtem o valor da entrada analógica e converte para 1 ou 0 (1 > 800, max 1023 3.3v)
+#  - para esta função usar divisor de tensão na entrada analógica para reduzir a tensão para até no máximo 3.3v
+
+def readadc(
+        adcnum):  # Faz a leitura da porta analógica e retorna 1 caso a leitura for mair que 800 e zero se for menor.
+    if ((adcnum > 7) or (adcnum < 0)):
+        return -1
+    r = spi.xfer2([1, (8 + adcnum) << 4, 0])
+    adcout = ((r[1] & 3) << 8) + r[2]
+    if (adcout > 600):
+        adcout = 1
+    else:
+        adcout = 0
+    return adcout
+
+
+def get_cpu_temp():  # retorna o valor da temperatura da cpu do CLP
+    tempFile = open("/sys/class/thermal/thermal_zone0/temp")
+    cpu_temp = tempFile.read()
+    tempFile.close()
+    return float(cpu_temp) / 1000
+
+
+def saudacao():
+
+    if (h < 12):
+
+        print("Bom dia")
+
+        pygame.mixer.music.load('mp3/010.mp3')  # Bom dia
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+    if (h >= 12 and h < 18):
+
+        print("Boa tarde")
+
+        pygame.mixer.music.load('mp3/009.mp3')  # Boa tarde
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+    if (h >= 18):
+
+        print("Boa noite")
+
+        pygame.mixer.music.load('mp3/150.mp3')  # Boa noite
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            time.sleep(0.1)
+
+def reproduz_som():
+
+    pygame.mixer.music.load('mp3/049.mp3')  # Espere o fechamento do primeiro portão
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        time.sleep(0.1)
+
+class Reles():
+
+    def __init__(self):
+
+        self.saidaA = 0b00000000  # Zera as saidas do port A (saidas do rele 1 ao rele 8 )
+        self.saidaB = 0b00000000  # Zera as saidas do port B (saidas dos reles 9 e 10 e dos transistors 11,12,13)
+        self.rele1 = False
+        self.rele2 = False
+        self.rele3 = False
+        self.rele4 = False
+        self.rele5 = False
+        self.rele6 = False
+        self.rele7 = False
+        self.rele8 = False
+        self.rele9 = False
+        self.rele10 = False
+        self.rele11 = False
+        self.rele12 = False
+        self.rele13 = False
+        
+        bus.write_byte_data(MCP23017, 0x015, 0)  # Zera saidas do port B
+        bus.write_byte_data(MCP23017, 0x014, 0)  # Coloca todas as saidas do PORT A em 0
+
+    def get_status1(self):
+        rele1 = self.rele1
+        return rele1
+    def get_status2(self):    
+        rele2 = self.rele2
+        return rele2
+    def get_status3(self):
+        rele3 = self.rele3
+        return rele3
+    def get_status4(self):
+        rele4 = self.rele4
+        return rele4
+    def get_status5(self):
+        rele5 = self.rele5
+        return rele5
+    def get_status6(self):
+        rele6 = self.rele6
+        return rele6
+    def get_status7(self):
+        rele7 = self.rele7
+        return rele7
+    def get_status8(self):
+        rele8 = self.rele8
+        return rele8
+    def get_status9(self):
+        rele9 = self.rele9
+        return rele9
+    def get_status10(self):
+        rele10 = self.rele10
+        return rele10
+    def get_status11(self):
+        rele11 = self.rele11
+        return rele11
+    def get_status12(self):
+        rele12 = self.rele12
+        return rele12
+    def get_status13(self):    
+        rele13 = self.rele13
+        return rele13
+       
+   
+    def rele1_on(self):
+        if self.rele1 == False:
+            self.saidaA = self.saidaA + 0b00000001  # aciona rele 1
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele1 = True
+
+    def rele1_off(self):
+        if self.rele1 == True:        
+            self.saidaA = self.saidaA - 0b00000001  # desliga rele 1
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele1 = False
+
+    def rele2_on(self):
+        if self.rele2 == False:
+            self.saidaA = self.saidaA + 0b00000010  # aciona rele 2 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele2 = True
+
+    def rele2_off(self):
+        if self.rele2 == True:
+            self.saidaA = self.saidaA - 0b00000010  # aciona rele 2 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele2 = False
+    
+    def rele3_on(self):
+        if self.rele3 == False:
+            self.saidaA = self.saidaA + 0b00000100  # aciona rele 3 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele3 = True
+
+    def rele3_off(self):
+        if self.rele3 == True:
+            self.saidaA = self.saidaA - 0b00000100  # aciona rele 3 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele3 = False
+    
+    def rele4_on(self):
+        if self.rele4 == False:
+            self.saidaA = self.saidaA + 0b00001000  # aciona rele 4 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele4 = True
+        
+    def rele4_off(self):
+        if self.rele4 == True:
+            self.saidaA = self.saidaA - 0b00001000  # aciona rele 4 
+            bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+            self.rele4 = False
+       
+    def rele5_on(self):
+        self.saidaA = self.saidaA + 0b00010000   # aciona rele 5
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele5_off(self):
+        self.saidaA = self.saidaA - 0b00010000  
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele6_on(self):
+        self.saidaA = self.saidaA + 0b00100000   # aciona rele 6
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele6_off(self):
+        self.saidaA = self.saidaA - 0b00100000  
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele7_on(self):
+        self.saidaA = self.saidaA + 0b01000000   # aciona rele 7
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele7_off(self):
+        self.saidaA = self.saidaA - 0b01000000  
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele8_on(self):
+        self.saidaA = self.saidaA + 0b10000000  # aciona rele 8
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele8_off(self):
+        self.saidaA = self.saidaA - 0b10000000  
+        bus.write_byte_data(MCP23017, 0x014, self.saidaA)
+    
+    def rele9_on(self):
+        self.saidaB = self.saidaB + 0b00000001  # aciona rele 9 
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele9_off(self):
+        self.saidaB = self.saidaB - 0b00000001  
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele10_on(self):
+        self.saidaB = self.saidaB + 0b00000010  # aciona rele 10 
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele10_off(self):
+        self.saidaB = self.saidaB - 0b00000010 
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele11_on(self):
+        self.saidaB = self.saidaB + 0b00000100  # liga LED AZUL saida GPB3
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele11_off(self):
+        self.saidaB = self.saidaB - 0b00000100  
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele12_on(self):
+        self.saidaB = self.saidaB + 0b00001000  # desliga LED VERMELHO saida GPB3
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele12_off(self):
+        self.saidaB = self.saidaB - 0b00001000  
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele13_on(self):
+        self.saidaB = self.saidaB + 0b00010000  # liga cooler
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+    
+    def rele13_off(self):
+        self.saidaB = self.saidaB - 0b00010000  
+        bus.write_byte_data(MCP23017, 0x015, self.saidaB)
+     
+
+   
+
+    
+
+
---
+++ app/views.py
@@ -0,0 +1,504 @@
+# -*- encoding: utf-8 -*-
+"""
+License: MIT
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Python modules
+import os, logging 
+import random
+# Flask modules
+from flask               import render_template, request, url_for, redirect, send_from_directory
+from flask_login         import login_user, logout_user, current_user, login_required
+from werkzeug.exceptions import HTTPException, NotFound, abort
+
+# App modules
+from app        import app, lm, db#, bc
+from app.models import User
+from app.forms  import LoginForm, RegisterForm
+import socket
+
+from gtts import gTTS  # importamos o modúlo gTTS
+import pygame  # Permite a utilização do modulo para execução de mp3
+import time  # Funções de tempo
+import RPi.GPIO as GPIO
+import smbus  # para funcionamento dos módulos com interface I2C
+import spidev
+import os  # Permite a execução de comandos do sistema operacional dentro do script Ex.: os.system('sudo reboot now')
+import threading  # Modulo superior Para executar as threads
+
+from app.biblioteca_SEA import Reles
+
+# from flask import current_app
+# from flask.signals import Namespace
+
+# namespace = Namespace()
+# message_sent = namespace.signal('mail_sent')
+
+# def message_response(recipient, body):
+#     ...
+#     message_sent.send(
+#         current_app._get_current_object(),
+#         recipient=recipient,
+#         body=body
+#     )
+
+# @message_sent.connect
+# def log_message(app, recipient, body):
+#     ...
+
+GPIO.setwarnings(False)  # desabilita mensagens de aviso
+GPIO.setmode(GPIO.BCM)  # Modo de endereço dos pinos BCM
+spi = spidev.SpiDev()  # Parametros da configuração da comunicação SPI (Entrada Portas Analógcas)
+spi.open(0, 0)
+spi.max_speed_hz = 1000000
+pygame.init()  # Inicia o pygame para uso do módulo de voz
+pygame.mixer.music.set_volume(0.1)  # Ajusta o volume geral das narrações do modulo de voz
+bus = smbus.SMBus(1)  # Parametros de configuração do módulo MCP23017 - SAIDA DOS RELÊS via I2C
+MCP23017 = 0X20  # Endereço do módulo de saidas dos reles
+MCP3008 = 0
+bus.write_byte_data(MCP23017, 0x00, 0x00)  # defina todo GPA como saida 0x00
+bus.write_byte_data(MCP23017, 0x01, 0x00)  # defina todo GPB como saida 0x01
+
+rele = Reles()
+
+# provide login manager with load_user callback
+@lm.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Logout user
+@app.route('/logout.html')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+# Register a new user
+@app.route('/register.html', methods=['GET', 'POST'])
+def register():
+    
+    # cut the page for authenticated users
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    # declare the Registration Form
+    form = RegisterForm(request.form)
+
+    msg = None
+
+    if request.method == 'GET': 
+
+        return render_template( 'pages/register.html', form=form, msg=msg )
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+        email    = request.form.get('email'   , '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        # filter User out of database through username
+        user_by_email = User.query.filter_by(email=email).first()
+
+        if user or user_by_email:
+            msg = 'Error: User exists!'
+        
+        else:         
+
+            pw_hash = password #bc.generate_password_hash(password)
+
+            user = User(username, email, pw_hash)
+
+            user.save()
+
+            msg = 'User created, please <a href="' + url_for('login') + '">login</a>'     
+
+    else:
+        msg = 'Input error'     
+
+    return render_template( 'pages/register.html', form=form, msg=msg )
+
+# Authenticate user
+@app.route('/login.html', methods=['GET', 'POST'])
+def login():
+    
+    # cut the page for authenticated users
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+            
+    # Declare the login form
+    form = LoginForm(request.form)
+
+    # Flask message injected into the page, in case of any errors
+    msg = None
+
+    # check if both http method is POST and form is valid on submit
+    if form.validate_on_submit():
+
+        # assign form data to variables
+        username = request.form.get('username', '', type=str)
+        password = request.form.get('password', '', type=str) 
+
+        # filter User out of database through username
+        user = User.query.filter_by(user=username).first()
+
+        if user:
+            
+            #if bc.check_password_hash(user.password, password):
+            if user.password == password:
+                login_user(user)
+                return redirect(url_for('index'))
+            else:
+                msg = "Wrong password. Please try again."
+        else:
+            msg = "Unknown user"
+
+    return render_template( 'pages/login.html', form=form, msg=msg )
+
+@app.route('/temperatura',methods=['GET'])
+def temperatura():
+    temp = os.popen("vcgencmd measure_temp").readline()
+        
+    for linha in os.popen("df -h /home/pi"):
+        linha = linha    
+        
+    temp = temp.split("=")[1]
+    temp = temp.split(".")[0] 
+    
+    dados = {"temperatura":temp}    
+
+    return dados
+
+@app.route('/',methods=['GET', 'POST'])
+def index():
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(hostname)
+
+    temp = os.popen("vcgencmd measure_temp").readline()
+    mem = os.popen("vcgencmd get_mem arm").readline()
+    
+    for linha in os.popen("df -h /home/pi"):
+        linha = linha
+    
+    linha = linha.split(" ")
+    linha = linha[-2]
+    linha = str(linha)
+    hd = linha.replace("%","")
+    
+    temp = temp.split("=")[1]
+    temp = temp.split(".")[0]
+    mem = mem.split("=")[1]
+    mem = mem.replace("M","")
+    mem = int(mem)
+    mem = round(mem/10)
+    
+    print("temperatura cpu",temp)
+    print("memoria",mem)
+    print("Espaço utilizado",hd)
+    
+    
+    temperatura = temp
+    espaco = hd
+    memoria = mem
+    ip = os.popen('hostname -I').readline()
+    ip = str(ip)
+    ip = ip.replace("\n","")
+    ip = ip.split(" ")
+    ip1 = ip[0]
+    ip2 = ip[1]
+    print("Nome desta maquina",ip1,ip2)
+
+    return render_template( 'pages/index.html', temperatura=temperatura, ip = ip1,
+                            espaco = espaco, memoria = memoria)
+
+
+@app.route('/atualiza',methods=['GET'])
+def atualiza():
+    r = request.args
+    print("Recebido na rota Atualiza",r)   
+ 
+    if "rele1" in r:
+        print("reconheceu rele1 na chamada")
+        rele1 = rele.get_status1()
+        dados = {"rele1" : rele1}
+        print("dados a enviar",dados)
+
+    if "rele2"in r:
+        rele2 = rele.get_status2()
+        dados = {"rele2":rele2}
+
+    if "rele3"in r:
+        rele3 = rele.get_status3()
+        dados = {"rele3":rele3}
+
+    if "rele4"in r:
+        rele4 = rele.get_status4()
+        dados = {"rele4":rele4}
+
+    if "rele5"in r:
+        rele5 = rele.get_status5()
+        dados = {"rele5":rele5}
+
+    if "rele6"in r:
+        rele6 = rele.get_status6()
+        dados = {"rele6":rele6}
+
+    if "rele7"in r:
+        rele7 = rele.get_status7()    
+        dados = {"rele7":rele7}
+
+    if "rele8"in r:
+        rele8 = rele.get_status8()    
+        dados = {"rele8":rele8}
+
+    if "rele9"in r:
+        rele9 = rele.get_status9()    
+        dados = {"rele9":rele9}
+
+    if "rele10"in r:
+        rele10 = rele.get_status10()    
+        dados = {"rele10":rele10}
+
+    if "rele11"in r:
+        rele11 = rele.get_status11()    
+        dados = {"rele11":rele11}
+
+    if "rele12"in r:
+        rele12 = rele.get_status12()    
+        dados = {"rele12":rele12}
+
+    if "rele13"in r:
+        rele13 = rele.get_status13()
+        dados = {"rele13":rele13}    
+    
+    if "todos" in r:
+        print("Atualizando todos os status dos reles")
+        rele1 = rele.get_status1()
+        rele2 = rele.get_status2()
+        rele3 = rele.get_status3()
+        rele4 = rele.get_status4()
+        rele5 = rele.get_status5()
+        rele6 = rele.get_status6()
+        rele7 = rele.get_status7()
+        rele8 = rele.get_status8()
+        rele9 = rele.get_status9()
+        rele10 = rele.get_status10()
+        rele11 = rele.get_status11()
+        rele12 = rele.get_status12()
+        rele13 = rele.get_status13()
+
+        dados = {"rele1" : rele1,
+                 "rele2" : rele2,
+                 "rele3" : rele3,
+                 "rele4" : rele4,
+                 "rele5" : rele5,
+                 "rele6" : rele6,
+                 "rele7" : rele7,
+                 "rele8" : rele8,
+                 "rele9" : rele9,
+                 "rele10" : rele10,
+                 "rele11" : rele11,
+                 "rele12" : rele12,
+                 "rele13" : rele13}
+    
+    return dados
+
+@app.route('/reles',methods=['GET', 'POST'])
+def reles():
+    
+    r = request.args
+    print("Argumentos recebidos",r)
+        
+    if "switch1_on" in r:           
+        rele.rele1_on()        
+    if 'switch1_off' in r:        
+        rele.rele1_off()
+        
+    if "switch2_on" in r:           
+        rele.rele2_on()
+    if 'switch2_off' in r:        
+        rele.rele2_off()
+
+    if "switch3_on" in r:           
+        rele.rele3_on() 
+    if 'switch3_off' in r:        
+        rele.rele3_off()
+
+    if "switch4_on" in r:           
+        rele.rele4_on() 
+    if 'switch4_off' in r:        
+        rele.rele4_off()
+
+    if "switch5_on" in r:           
+        rele.rele5_on()
+    if 'switch5_off' in r:        
+        rele.rele5_off()
+
+    if "switch6_on" in r:           
+        rele.rele6_on()
+    if 'switch6_off' in r:        
+        rele.rele6_off()
+
+    if "switch7_on" in r:           
+        rele.rele7_on()
+    if 'switch7_off' in r:        
+        rele.rele7_off()
+
+    if "switch8_on" in r:           
+        rele.rele8_on() 
+    if 'switch8_off' in r:        
+        rele.rele8_off()
+
+    if "switch9_on" in r:           
+        rele.rele9_on()
+    if 'switch9_off' in r:        
+        rele.rele9_off()
+
+    if "switch10_on" in r:           
+        rele.rele10_on() 
+    if 'switch10_off' in r:        
+        rele.rele10_off()
+
+    if "switch11_on" in r:           
+        rele.rele11_on() 
+    if 'switch11_off' in r:        
+        rele.rele11_off()
+
+    if "switch12_on" in r:           
+        rele.rele12_on()
+    if 'switch12_off' in r:        
+        rele.rele12_off()    
+        
+    if "switch13_on" in r:         
+        rele.rele13_on() 
+    if 'switch13_off' in r:        
+        rele.rele13_off() 
+
+    if "rele1" in r:
+        rele.rele1_on()     
+        time.sleep(3)
+        rele.rele1_off()
+        # session['rele1'] = False 
+
+    if "rele2" in r:
+        rele.rele2_on()     
+        time.sleep(3)
+        rele.rele2_off() 
+
+    if "rele3" in r:
+        rele.rele3_on()     
+        time.sleep(3)
+        rele.rele3_off() 
+
+    if "rele4" in r:
+        rele.rele4_on()     
+        time.sleep(3)
+        rele.rele4_off() 
+
+    if "rele5" in r:        
+        rele.rele5_on()     
+        time.sleep(3)
+        rele.rele5_off()
+        
+    if "rele6" in r:
+        rele.rele6_on()     
+        time.sleep(3)
+        rele.rele6_off()
+
+    if "rele7" in r:
+        rele.rele7_on()     
+        time.sleep(3)
+        rele.rele7_off()
+
+    if "rele8" in r:
+        rele.rele8_on()     
+        time.sleep(3)
+        rele.rele8_off() 
+
+    if "rele9" in r:
+        rele.rele9_on()     
+        time.sleep(3)
+        rele.rele9_off() 
+
+    if "rele10" in r:
+        rele.rele10_on()     
+        time.sleep(3)
+        rele.rele10_off()    
+
+    if "rele11" in r:
+        rele.rele11_on()     
+        time.sleep(3)
+        rele.rele11_off()
+
+    if "rele12" in r:
+        rele.rele12_on()     
+        time.sleep(3)
+        rele.rele12_off()         
+
+    if "rele13" in r:
+        rele.rele13_on()     
+        time.sleep(3)
+        rele.rele13_off()
+
+    if "reiniciar" in r:
+        print("REINICIANDO O SISTEMA...")
+        os.system("sudo reboot now") 
+    
+       
+    dados = {"rele" : "ok"}
+                # "rele2" : rele2,
+                # "rele3" : rele3,
+                # "rele4" : rele4,
+                # "rele5" : rele5,
+                # "rele6" : rele6,
+                # "rele7" : rele7,
+                # "rele8" : rele8,
+                # "rele9" : rele9,
+                # "rele10" : rele10,
+                # "rele11" : rele11,
+                # "rele12" : rele12,
+                # "rele13" : rele13,
+                # "rele1" : rele1,
+                # "rele1" : rele1,
+                # "rele1" : rele1,
+                # "rele1" : rele1,
+                # "rele1" : rele1,
+                # "temperatura" : temperatura,
+                # "espaco" : espaco,
+                # "memoria" : memoria                 
+                #  }
+
+    return dados
+
+       
+    
+@app.route('/', defaults={'path': 'index_original.html'})
+@app.route('/<path>')
+def index2(path):
+
+    if not current_user.is_authenticated:
+        return redirect(url_for('login'))
+
+    content = None
+
+    try:
+
+        # try to match the pages defined in -> pages/<input file>
+        return render_template( 'pages/'+path )
+    
+    except:
+        
+        return render_template( 'pages/error-404.html' )
+
+# Return sitemap 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'sitemap.xml')
